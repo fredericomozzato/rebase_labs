c2ca95bc7395087de55ab0a7f29fbafe81fd930c
Frederico <frederico.smozzato@gmail.com>
2024-03-08 10:09:11 -0300

    Adiciona tabelas no script de inicialização

--- backend/persistence/init.sql
+++ backend/persistence/init.sql
@@ -1,21 +1,47 @@
 CREATE DATABASE relabs_test;
 
+CREATE TABLE IF NOT EXISTS patients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  cpf CHAR(14) NOT NULL UNIQUE,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  birthdate DATE NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS doctors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  crm CHAR(10) NOT NULL UNIQUE,
+  crm_state CHAR(2) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS tests (
   id SERIAL PRIMARY KEY,
-  patient_cpf CHAR(14),
-  patient_name VARCHAR(50),
-  patient_email VARCHAR(50),
-  patient_birthdate DATE,
-  patient_address VARCHAR(100),
-  patient_city VARCHAR(50),
-  patient_state VARCHAR(20),
-  doctor_crm CHAR(10),
-  doctor_crm_state CHAR(2),
-  doctor_name VARCHAR(50),
-  doctor_email VARCHAR(50),
-  test_result_token CHAR(6),
-  test_date DATE,
-  test_type VARCHAR(20),
-  test_type_range VARCHAR(10),
-  test_result INTEGER
+  token CHAR(6) NOT NULL UNIQUE,
+  date DATE NOT NULL,
+  patient_id INTEGER,
+  doctor_id INTEGER,
+  
+  CONSTRAINT fk_patient 
+    FOREIGN KEY(patient_id)
+      REFERENCES patients(id),
+  CONSTRAINT fk_doctor
+    FOREIGN KEY(doctor_id)
+      REFERENCES doctors(id)
+);
+
+CREATE TABLE IF NOT EXISTS test_types (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(20) NOT NULL,
+  type_range VARCHAR(10) NOT NULL,
+  result INTEGER NOT NULL,
+  test_id INTEGER,
+  
+  CONSTRAINT fk_test
+    FOREIGN KEY(test_id)
+      REFERENCES tests(id)
 );
